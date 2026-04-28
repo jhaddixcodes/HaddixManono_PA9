4aa5652621ebe8b8ae796253e3d86c42e1368196
jhaddixcodes <jameshaddix1110@gmail.com>
2026-04-27 20:34:15 -0700

    Our code! It's broken! fixed now

--- src/game_logic/Hand.cpp
+++ src/game_logic/Hand.cpp
@@ -54,5 +54,30 @@ bool Hand::isBust() const
 
 bool Hand::isTwentyOne() const
 {
-	return (calculatePoints() == 21);
+	// when checking for 21 at the beginning of the round we disregard any hidden cards but other than that it's basically calculatePoints()
+	
+	int points = 0;
+
+	// this will let us calculate 1 vs. 11
+	int numAces = 0;
+
+	// iterate through all cards in the hand
+	for (const Card& card : m_cards)
+	{
+		// we keep track of how many aces so we can subtract 10s later
+		if (card.getSymbol() == "A")
+		{
+			numAces++;
+		}
+		points += card.getCardValue();
+	}
+
+	// if points are above 21, subtract 10 until we're out of aces. if it's above 21 still, bwomp
+	while (points > 21 && numAces > 0)
+	{
+		points -= 10;
+		numAces--;
+	}
+
+	return points == 21;
 }
